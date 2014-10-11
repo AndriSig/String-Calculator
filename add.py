ce29c1a075029ac5b9fae47b94c86bd2404b45a8
Garviell <faustusxi@gmail.com>
2014-10-11 23:05:20 +0000

    added a check to throw an exception if a negative number is found, all test cases now green

--- add.py
+++ add.py
@@ -2,11 +2,24 @@
 import sys
 import re
 import unittest
+import string
 #Takes in a string or strings
 def add(numbers):
     result = 0
-    endl = re.compile(',|\n')
+    delim = ','
+    if numbers[:2] == '//':
+        if numbers[2] in string.punctuation:
+            delim = '\\' + numbers[2]
+        else:
+            delim = numbers[2]
+    endl = re.compile('{0}|\n'.format(delim))
     for number in re.split(endl,numbers):
+        if '-' in number:
+            matches = re.findall('-\d*', numbers)
+            errorString = "Negatives not allowed: "
+            for negative in matches:
+                errorString += negative +','
+            raise ValueError(errorString)
         if number.isdigit():
             result += int(number)
     return result
