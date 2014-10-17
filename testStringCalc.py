84f9e5eab63e24e444b402dc607043651776208c
Garviell <faustusxi@gmail.com>
2014-10-17 13:40:05 +0000

    Add failing tests using new line as a delimiter

--- testStringCalc.py
+++ testStringCalc.py
@@ -17,14 +17,32 @@ class BasicAdditionTest(unittest.TestCase):
     #A string with two numbers using , as delimiter should return the sum of those numbers
     def testOneTwoStr(self):
         self.assertEqual(stringCalculator.add("1,2"),3)
-
+    #Test 4
+    #New line should be interchangable with comma as a delimiter
+    def testOneEndl(self):
+        self.assertEqual(stringCalculator.add("1\n2,3"), 6)
+    #Test 5
+    #New line should be able to entirely replace comma as a delimiter
+    def testTwoEndl(self):
+        self.assertEqual(stringCalculator.add("1\n2\n3"), 6)
+    #Test 6
+    #Stress Testing newline as delimiter
+    def testStaggeredEndl(self):
+        sum = ""
+        delim = [',','\n']
+        for i in range(1, 100001):
+            sum += '{0}{1}'.format(i,delim[i%2])
+        self.assertEqual(stringCalculator.add(sum),5000050000)
 class AdditionRangeTest(unittest.TestCase):
+    #Test 1
+    #Sum series from 1 to 100 results in 5050
     def testHundred(self):
         sum = ""
         for i in range(1,101):
             sum += '{0},'.format(i)
         self.assertEqual(stringCalculator.add(sum),5050)
-    
+    #Test 2
+    #Sum series from 1 to 100000 results in 5000050000
     def testHundredK(self):
         sum = ""
         for i in range (1,100001):
