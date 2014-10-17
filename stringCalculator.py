b99903f01e8c4a1d08bec124d4f38dd2ed0033f5
Garviell <faustusxi@gmail.com>
2014-10-17 13:41:42 +0000

    added the functionality to use new lines as a delimiter, all tests are now green

--- stringCalculator.py
+++ stringCalculator.py
@@ -6,7 +6,7 @@ import string
 
 def add(numbers):
     result = 0
-    for num in re.split(r',', numbers):
+    for num in re.split(r',|\n', numbers):
         if num.isdigit():
             result += int(num)
     return result
