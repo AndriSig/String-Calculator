7325a10c3b66e2564c7d4e3697488475c05dc877
Garviell <faustusxi@gmail.com>
2014-10-17 16:10:05 +0000

    Added the function raiseNeg to be called if a number is negative, All tests now green

--- stringCalculator.py
+++ stringCalculator.py
@@ -11,9 +11,18 @@ def add(numbers):
         delim = re.compile(findDelim(numbers))
         numbers = re.sub(r'^[^\d]*','', numbers)
     for num in re.split(delim, numbers):
+        if '-' in num:
+            raiseNeg(numbers)
         if num.isdigit():
             result += int(num)
     return result
   
 def findDelim(input):
     return re.escape(input[2])
+
+def raiseNeg(numbers):
+    matches = re.findall('-\d*', numbers)
+    errorString = "Negatives not allowed: "
+    for negative in matches:
+        errorString += negative +','
+    raise ValueError(errorString)
