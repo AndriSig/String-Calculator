532b1ee87af126e16755e2da1e62a99dcc6b700f
Garviell <faustusxi@gmail.com>
2014-10-18 15:30:08 +0000

    Ran coverage on the program and found that the tests have 100% line coverage.  But i also found out that one test fails when run on pyCharm, i think it may be because re.escape works differently between python 2.7 and 3

--- stringCalculator.py
+++ stringCalculator.py
@@ -3,7 +3,7 @@ import sys
 import re
 import unittest
 import string
-
+#Takes in a string of numbers and gives their sum
 def add(numbers):
     result = 0
     delim = ',|\n'
@@ -16,7 +16,7 @@ def add(numbers):
         if num.isdigit() and int(num) < 1001:
             result += int(num)
     return result
-  
+ #Takes in a string of numbers and gives the delimiter/s at the start of said string 
 def findDelim(input):
     input = re.findall(r'^[^\d]*', input)[0]
     if re.match(r'//(\[.*?\])', input) is not None:
@@ -31,7 +31,7 @@ def findDelim(input):
             return re.escape(delim[0])
     else:
         return re.escape(input[2])
-
+#Raises an exception if called
 def raiseNeg(numbers):
     matches = re.findall('-\d*', numbers)
     errorString = "Negatives not allowed: "
